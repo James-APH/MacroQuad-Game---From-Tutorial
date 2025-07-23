094edf7f34c03a89c7cadd06084b09fa99759d97
James Huston <jamzhuston@gmail.com>
2025-07-23 00:00:23 -0600

    created and started to implement enemy file

---
+++ src/enemy.rs
@@ -0,0 +1,28 @@
+use crate::game_objects::*;
+use macroquad::prelude::*;
+
+struct Square {
+    body: Shape,
+    color: Color,
+}
+
+pub fn init_square(shape_body: Shape, color_type: Color) -> Square {
+    Square {
+        body: shape_body,
+        color: color_type,
+    }
+}
+
+impl Update for Square {
+    fn update(self: &mut Square, dt: f32) {
+        let distance = self.body.get_speed() * dt;
+        self.body.set_y(distance);
+    }
+}
+
+impl Draw for Square {
+    fn draw(self: &Square) {
+        let apothem: f32 = self.body.get_size() / 2.0;
+        draw_circle(self.body.get_x(), self.body.get_y(), apothem, self.color);
+    }
+}
