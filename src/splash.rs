d0fc8af70ff4322a6a5e908e6495e23e42aace80
James Huston <jamzhuston@gmail.com>
2025-07-24 15:34:07 -0600

    commented out code

---
+++ src/splash.rs
@@ -0,0 +1,12 @@
+/*
+/// GAME_OVER SPLASH_SCREEN
+let text = "GAME OVER!";
+            let text_dimensions = measure_text(text, None, 50, 1.0);
+            draw_text(
+                text,
+                screen_width() / 2.0 - text_dimensions.width / 2.0,
+                screen_height() / 2.0,
+                50.0,
+                RED,
+            );
+*/
